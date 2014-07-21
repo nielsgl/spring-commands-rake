b3a50796d933c4dba3d2e56462970645e7adc620
Niels van Galen Last <nvangalenlast@gmail.com>
2014-07-21 15:28:07 -0700

    Implemented basic spring matcher

--- lib/spring/commands/rake.rb
+++ lib/spring/commands/rake.rb
@@ -2,16 +2,30 @@ require "spring/commands/rake/version"
 
 module Spring
 	module Commands
-		class Rake
+		class RakeMagic
 
 			class << self
 				attr_accessor :environment_matchers
 			end
 
-			# Your code goes here...
-			def self.hello_world
-				puts "hi!"
+			self.environment_matchers = {
+				default: 'test',
+				/^test($|:)/ => 'test'
+			}
+
+			def env(args)
+				if env = args.grep(/^(RAILS|RACK)_ENV=(.*)$/m).last
+					return env.split("=").last
+				end
+
+				self.class.environment_matchers.each do |matcher, environment|
+					return environment if matcher === (args.first || :default)
+				end
+
+				nil
 			end
+
 		end
+		Spring.register_command "rake_magic", RakeMagic.new
 	end
 end
