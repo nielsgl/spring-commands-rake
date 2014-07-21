20b22e1af6ca76d052fa3b492574843288f29c6f
Niels van Galen Last <nvangalenlast@gmail.com>
2014-07-21 15:06:01 -0700

    initial version

--- lib/spring/commands/rake.rb
+++ lib/spring/commands/rake.rb
@@ -1,9 +1,12 @@
 require "spring/commands/rake/version"
 
 module Spring
-  module Commands
-    module Rake
-      # Your code goes here...
-    end
-  end
+	module Commands
+		module Rake
+			# Your code goes here...
+			def hello_world
+				puts "hi!"
+			end
+		end
+	end
 end
